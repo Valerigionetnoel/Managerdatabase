60e5e87b3579a6e372abcae163c50cd7be2a7d9c
Valéri Gionet-Noël <valeri.gionetnoel@gmail.com>
2023-04-11 14:46:08 -0300

    done the database

---
+++ db/schema.sql
@@ -0,0 +1,29 @@
+CREATE DATABASE IF NOT EXISTS manager_db;
+
+USE manager_db
+
+CREATE TABLE department (
+   id INT PRIMARY KEY,
+   _name VARCHAR(30),
+)
+
+CREATE TABLE role (
+    id INT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
+)
+
+CREATE TABLE employee (
+    id: PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+    ON DELETE SET NULL
+)
